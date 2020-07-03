cfaf35a8db0de1e245232d8fbacce9592886265d
Desputicus <miguel_g14@hotmail.com>
2020-07-02 23:25:36 -0400

    arregaldo el historial

--- database/views/historialPacientes.sql
+++ database/views/historialPacientes.sql
@@ -8,7 +8,22 @@ SELECT 	TR.id_registro
         ,TP.fecha_nacimiento
 		,TNE.NOMBRE_NIVEL_ENFERMEDAD
 		,TR.fecha_registro
+        ,TR.observaciones
+        ,S.id_sintomas
+        ,S.dolor_cabeza
+        ,S.estornudos
+        ,S.fiebre 
+		,S.fatiga 
+		,S.tos_seca 
+		,S.dificultad_respirar 
+		,S.dolor_cuerpo 
+		,S.sin_olores 
+		,S.dolor_garganta 
+		,S.embarazo 
+		,S.fecha_sintomas 
+		,S.alergias
 FROM registro TR
 INNER JOIN PERSONA TP ON TR.id_persona = TP.id_persona
 INNER JOIN TIPO_REGISTRO TREG ON TR.ID_TIPO_REGISTRO = TREG.ID_TIPO_REGISTRO
-INNER JOIN NIVEL_ENFERMEDAD TNE ON TR.id_nivel_enfermedad = TNE.id_nivel_enfermedad 
+INNER JOIN NIVEL_ENFERMEDAD TNE ON TR.id_nivel_enfermedad = TNE.id_nivel_enfermedad
+INNER JOIN SINTOMAS S ON TR.id_sintomas = S.id_sintomas
